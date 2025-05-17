76cb7b1ec20063477adddb38d0cd075fb53ff4d5
Abhimanyu <b22bb001@iitj.ac.in>
2025-05-17 20:26:51 +0530

    second

---
+++ first.js
@@ -0,0 +1,16 @@
+const button ="Added a button"
+console.log(button)
+
+const login ="login";
+console.log(login)
+
+const footer = "footer"
+console.log(footer)
+const payment = "INTEGRATED PAYMENT"
+console.log(payment);
+
+
+const upi = "upi added"
+console.log(upi)
+
+console.log("Latest commit")
